895e3841c8361cdfb28ab9ef822ff6a7efd3820b
CJCU-CC <108B01227@mailst.cjcu.edu.tw>
2021-03-23 11:34:58 +0800

    test1

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+def index(request, tvno = 0):
+    tv_list = [{'name':'東森', 'tvcode':'R2iMq5LKXco'},
+               {'name':'民視', 'tvcode':'XxJKnDLYZz4'},
+               {'name':'公視', 'tvcode':'ED4QXd5xAco'},]
+    now = datetime.now()
+    hour = now.timetuple().tm_hour
+    tvno = tvno
+    tv = tv_list[tvno]
+    return render(request, 'index.html', locals())
